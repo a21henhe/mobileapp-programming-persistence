12f5cb8b64a6da72056b39ca54fe0ee140af389c
a21henhe <a21henhe@student.his.se>
2022-04-29 13:56:47 +0200

    added buttons and edittexts

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -3,13 +3,30 @@ package com.example.persistence;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private Button readButton;
+    private Button writeButton;
+    private EditText edit1;
+    private EditText edit2;
+    private EditText edit3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        readButton = findViewById(R.id.read);
+        writeButton = findViewById(R.id.write);
+        edit1 = findViewById(R.id.edit1);
+        edit2 = findViewById(R.id.edit2);
+        edit3 = findViewById(R.id.edit3);
     }
 
 
